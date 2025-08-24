875e4b0536078d888037c80fe406b838d27078ce
Loocist23 <anthonyzegnal05@gmail.com>
2025-08-24 14:16:59 +0200

    style: add static white grid on black background

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import BackgroundGrid from "@/components/BackgroundGrid";
 
 export const metadata: Metadata = {
   title: "Loocist23",
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className="flex flex-col min-h-screen">
-        <BackgroundGrid />
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
